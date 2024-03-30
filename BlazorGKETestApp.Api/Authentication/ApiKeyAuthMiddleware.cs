ffb64951e983d248d09523834ffafe4140817da4
Allard Schuurmans <a.schuurmans@4DotNet.nl>
2024-03-30 14:34:22 +0100

    test

---
+++ BlazorGKETestApp.Api/Authentication/ApiKeyAuthMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+
+namespace BlazorGKETestApp.Api.Authentication
+{
+    public class ApiKeyAuthMiddleware
+    {
+        private readonly RequestDelegate _requestDelegate;
+        private const string ApiKey = "X-API-KEY";
+
+        public ApiKeyAuthMiddleware(RequestDelegate requestDelegate)
+        {
+            _requestDelegate = requestDelegate;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (!context.Request.Headers.TryGetValue(ApiKey, out var apiKeyVal))
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                return;
+            }
+
+            var appSettings = context.RequestServices.GetRequiredService<IConfiguration>();
+            var apiKey = appSettings.GetValue<string>(ApiKey);
+            if (!apiKey.Equals(apiKeyVal))
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                return;
+            }
+
+            await _requestDelegate(context);
+        }
+    }
+}
